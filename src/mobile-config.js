5a0bdb70643050f00ffd2a1c40ecd053958f9c6d
Ivan Montiel <idmontie@asu.edu>
2015-12-06 09:55:45 -0700

    Get mobile ready

---
+++ src/mobile-config.js
@@ -0,0 +1,9 @@
+App.info({
+  id: 'com.mantarayar.lifewaster',
+  version: '1.0.0',
+  name: 'Life Waster',
+  description: 'Waste some time by clicking a button',
+  author: 'Ivan Montiel',
+  email: 'ivanmontiel@mantarayar.com',
+  website: 'https://github.com/idmontie/life-waster'  
+});
